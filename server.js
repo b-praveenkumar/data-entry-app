470e49f9dd1647fd0e84138f2db95eb66e65870b
b-praveenkumar <100297793+b-praveenkumar@users.noreply.github.com>
2024-02-19 12:03:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = 3001;
 const port = process.env.PORT || 3001;
 
 
